c5591aaf01a01bcf467beec12803b0321813944d
avros16 <114757828+avros16@users.noreply.github.com>
2024-07-28 15:19:35 +0000

    prompt1

--- .ccls-cache/@home@runner@practice-pointers-and-references/main.cpp
+++ .ccls-cache/@home@runner@practice-pointers-and-references/main.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
+using namespace std;
+
+void Swap(int* ptr1, int* ptr2){
+
+  int temp =*ptr2;
+  *ptr2=*ptr1;
+  *ptr1=temp;
+
+  
+}
+
+
 
 int main() {
-  std::cout << "Hello World!\n";
+
+  int one{1};
+  int two{2};
+
+  
+cout << one << "\n" << two << "\n";
+  
+  Swap(&one,&two);
+  
+  cout << one << "\n" << two;
+
+  
 }
